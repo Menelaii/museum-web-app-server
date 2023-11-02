592b9efb87baacbd903089e491f3035384782b82
d.zhezha <d.post2434@gmail.com>
2023-11-02 18:00:47 +0300

    edit basic exception handler

--- src/main/java/ru/solovetskyJungs/museum/exception/BasicExceptionHandler.java
+++ src/main/java/ru/solovetskyJungs/museum/exception/BasicExceptionHandler.java
@@ -1,13 +1,19 @@
 package ru.solovetskyJungs.museum.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class BasicExceptionHandler {
+    private final static Logger logger = LoggerFactory.getLogger(BasicExceptionHandler.class);
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiExceptionDTO> handle(Exception e) {
+        logger.error(e.toString() + " " + e.getMessage());
+
         return ResponseEntity.badRequest().body(
                 new ApiExceptionDTO(e, e.getMessage()
                 )
